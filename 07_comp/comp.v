e1d474a4f28cad32bb2b53d53e5640d27cf34d21
srilekha-dev <srilekhadveng@gmail.com>
2025-08-26 17:28:36 +0530

    Update comp.v

--- 07_comp/comp.v
+++ 07_comp/comp.v
@@ -1,1 +1,19 @@
+module comp (
+    input [1:0] A, B,
+    output reg AeqB, AgtB, AltB
+);
+
+always @(*) begin
+    if (A == B) begin
+        AeqB = 1; AgtB = 0; AltB = 0;
+    end
+    else if (A > B) begin
+        AeqB = 0; AgtB = 1; AltB = 0;
+    end
+    else begin
+        AeqB = 0; AgtB = 0; AltB = 1;
+    end
+end
+
+endmodule
 
