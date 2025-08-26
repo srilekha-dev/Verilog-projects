03686f7af0d95a79fef00ad80a22e9c32566c7a3
srilekha-dev <srilekhadveng@gmail.com>
2025-08-26 18:00:50 +0530

    Update full_adder.v

--- 09_full_adder/full_adder.v
+++ 09_full_adder/full_adder.v
@@ -1,1 +1,9 @@
+module full_adder (
+    input A, B, Cin,
+    output Sum, Cout
+);
 
+assign Sum  = A ^ B ^ Cin;                  // XOR for sum
+assign Cout = (A & B) | (B & Cin) | (A & Cin); // Majority function
+
+endmodule
