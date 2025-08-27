6d7b8606a029a6ac642ff87ce9f7f7e9b2d62a13
srilekha-dev <srilekhadveng@gmail.com>
2025-08-27 13:01:14 +0530

    Update sr_latch.v

--- 13_sr_latch/sr_latch.v
+++ 13_sr_latch/sr_latch.v
@@ -1,1 +1,24 @@
+module sr_latch(
+                input en,s,r,
+                output reg q,
+                output qn);
+assign qn=~q;
 
+always @(*)
+    begin
+        if(!en)
+                        q<=0;
+            
+        else
+               begin
+                        case({s,r})
+                        2'b00 : q<=q;
+                        2'b01 : q<=0;
+                        2'b10 : q<=1;
+                        default : q<=1'bx;
+                       endcase
+                      end
+                     
+                       end
+
+    endmodule
