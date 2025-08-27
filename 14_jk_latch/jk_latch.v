7fc0a6224d3528094b64d9568fdb93e6cf2f087d
srilekha-dev <srilekhadveng@gmail.com>
2025-08-27 12:50:06 +0530

    Update jk_latch.v

--- 14_jk_latch/jk_latch.v
+++ 14_jk_latch/jk_latch.v
@@ -1,1 +1,24 @@
+module jk_latch(
+                input en,j,k,
+                output reg q,
+                output qn);
+assign qn=~q;
 
+always @(*)
+    begin
+        if(!en)
+                        q<=0;
+            
+        else
+               begin
+                        case({j,k})
+                        2'b00 : q<=q;
+                        2'b01 : q<=0;
+                        2'b10 : q<=1;
+                        default : q=~q;
+                       endcase
+                      end
+                     
+                       end
+
+    endmodule
