37e3614cdf684a08082f0d1bdf450635f309883b
srilekha-dev <srilekhadveng@gmail.com>
2025-08-27 12:25:18 +0530

    Update t_latch.v

--- 12_t_latch/t_latch.v
+++ 12_t_latch/t_latch.v
@@ -1,1 +1,23 @@
+module t_latch(
+                input en,t,
+                output reg q,
+                output qn);
+assign qn=~q;
 
+always @(en or t)
+    begin
+        if(!en)
+            q<=0;
+        else
+            begin
+            case(t)
+            1'b0 : q<=q;
+            
+            default : q<=~q;
+            
+                       endcase
+                      end
+                                            
+                       end
+
+    endmodule
