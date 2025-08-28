5e715ecde5d8f6dc3d9f94fccddc69d4fe0767da
srilekha-dev <srilekhadveng@gmail.com>
2025-08-28 10:46:17 +0530

    Update d_ff.v

--- 15_d_ff/d_ff.v
+++ 15_d_ff/d_ff.v
@@ -1,1 +1,22 @@
+module d_ff(
+            input clk,rst,
+            input d,
+            output reg q,
+            output qn
+            );
+
+assign qn=~q;
+
+always @(posedge clk)
+
+
+    begin
+    if(!rst)
+       
+         q<=0;
+       
+     else
+         q<=d;
+    end
+endmodule
 
