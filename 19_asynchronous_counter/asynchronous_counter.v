b64a2c250a86c267ff347e92a4777c969e0689dd
srilekha-dev <srilekhadveng@gmail.com>
2025-09-05 18:29:44 +0530

    Update asynchronous_counter.v

--- 19_asynchronous_counter/asynchronous_counter.v
+++ 19_asynchronous_counter/asynchronous_counter.v
@@ -5,13 +5,26 @@ module async_counter_4bit (
 
     reg [3:0] count;
 
+    // Q0 toggles with CLK
+    always @(posedge CLK or posedge Reset)
+        if (Reset) count[0] <= 0;
+        else count[0] <= ~count[0];
+
+    // Next FFs toggle on inverted outputs to ensure UP counting
+    always @(negedge count[0] or posedge Reset)
+        if (Reset) count[1] <= 0;
+        else count[1] <= ~count[1];
+
+    always @(negedge count[1] or posedge Reset)
+        if (Reset) count[2] <= 0;
+        else count[2] <= ~count[2];
+
+    always @(negedge count[2] or posedge Reset)
+        if (Reset) count[3] <= 0;
+        else count[3] <= ~count[3];
+
     assign Q = count;
 
-    always @(posedge CLK or posedge Reset) begin
-        if (Reset)
-            count <= 4'b0000;      // Reset counter
-        else
-            count <= count + 1;    // Increment count
-    end
-
 endmodule
+
+
